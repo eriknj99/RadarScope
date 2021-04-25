Fixed Waterfall Display Scalling, Added range calculation

ModularScope.py
@@ -49,7 +49,7 @@ class ModularScope():
                      "\nPeak      : " + str(self.PEAKS[0]) + 
                      "\n----------" +
                      "\nVelocity  : ??? m/s"+
-                     "\nRange     : ??? m"
+                     "\nRange     : " + str(self.sp.getRanges()[0])
             )
 
         self.consoleTmr.timeout.connect(update)
@@ -88,10 +88,11 @@ class ModularScope():
         T = 8.7177e-3
 
         view = pg.PlotItem(invertY=True)
+        view.showGrid(x=True,y=True)
         self.win.addItem(view, colspan=20)
         img = pg.ImageItem(border='w')
-        pos = np.array([0., .25, 0.75, 1.0])
-        color = np.array([[0,0,128,255], [255,255,0,255], [0,0,0,255], (0, 0, 255, 255), (255, 0, 0, 255)], dtype=np.ubyte)
+        pos = np.array([0., 1., 0.5, 0.25, 0.75])
+        color = np.array([[0,0,128,128], [0, 0, 255, 255], [0,0,0,255], [255, 0, 0, 255],[255,255,0,255]], dtype=np.ubyte)
         cmap = pg.ColorMap(pos, color)
         lut = cmap.getLookupTable(0.0, 1.0, 10000)
         img.setLevels([0,10000])
@@ -103,17 +104,18 @@ class ModularScope():
         img.scale(((self.sp.SAMPLE_RATE/self.sp.FFT_SIZE) * C * T) / (4*B),1)
         
         def update():
-            img.setImage(np.flip(np.rot90(self.sp.ffts))[:int(self.sp.FFT_SIZE/2)])
+            img.setImage(np.flip(np.rot90(self.sp.ffts))[:int(self.sp.FFT_SIZE/2)], autoLevels=False)
             
         self.mainTmr.timeout.connect(update)
 
 
     def showFFTWaterfall(self, name):
         view = pg.PlotItem(invertY=True)
+        view.showGrid(x=True,y=True)
         self.win.addItem(view, colspan=20)
         img = pg.ImageItem(border='w')
         pos = np.array([0., 1., 0.5, 0.25, 0.75])
-        color = np.array([[0,255,255,0], [255,255,0,255], [0,0,0,255], (0, 0, 255, 255), (255, 0, 0, 255)], dtype=np.ubyte)
+        color = np.array([[0,0,128,128], [0, 0, 255, 255], [0,0,0,255], [255, 0, 0, 255],[255,255,0,255]], dtype=np.ubyte)
         cmap = pg.ColorMap(pos, color)
         lut = cmap.getLookupTable(0.0, 1.0, 10000)
         img.setLevels([0,10000])
@@ -126,7 +128,7 @@ class ModularScope():
         img.scale(self.sp.SAMPLE_RATE/self.sp.FFT_SIZE,1)
         
         def update():
-            img.setImage(np.flip(np.rot90(self.sp.ffts))[:int(self.sp.FFT_SIZE/2)])
+            img.setImage(np.flip(np.rot90(self.sp.ffts))[:int(self.sp.FFT_SIZE/2)], autoLevels=False)
             
         self.mainTmr.timeout.connect(update)
 

SignalProcessor.py
@@ -11,7 +11,8 @@ class SignalProcessor:
         self.fq.tick()
         self.bufferInFFT(FFT);     
         self.bufferInPeak(self.computePeak(FFT))
-        self.writeToFile(FFT) 
+        self.ranges = self.computeRanges()
+        #self.writeToFile(FFT) 
 
     def writeToFile(self,FFT):
         line = ""
@@ -56,7 +57,15 @@ class SignalProcessor:
     def getFFT(self):
         out =  np.array([(self.getHalfBinVals()), (self.getFrequencies())], dtype=float)
         return out
-    
+    def computeRanges(self):
+        C = 3e8
+        B = 100e6
+        T = 8.7177e-3
+        return self.getPeaks() * (((self.SAMPLE_RATE/self.FFT_SIZE) * C * T) / (4*B))
+
+    def getRanges(self):
+        return self.ranges
+
     # Appends the frequency of the peak to self.peaks for a single FFT entry 
     def computePeak(self, FFT):
         return self.binToFreq(5+np.argmax(FFT[5:int(self.FFT_SIZE/2)])) 
@@ -72,8 +81,8 @@ class SignalProcessor:
 
         self.OUTPUT_FILE = open("output.csv", "a")
         
-        self.MAX_FFTS = 100
-        self.MAX_PEAKS = 100
+        self.MAX_FFTS = 200
+        self.MAX_PEAKS = 200
 
         #self.numFFTs = 0
 
@@ -81,7 +90,7 @@ class SignalProcessor:
         self.fq = FrequencyCounter.FrequencyCounter()
 
         self.peaks = np.array([])
-
+        self.ranges = np.array([])
         self.ffts = np.array(self.MAX_FFTS*[np.zeros(self.ds.getFFTSize())])
 
         self.FFT_SIZE = self.ds.getFFTSize()